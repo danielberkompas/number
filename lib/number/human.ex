007daf9dc107164e1fa9faa828af97c69429052e
Steve Perkins <stereosteve@gmail.com>
2015-10-27 23:53:15 -0400

    wip: format numbers with SI notation
    TODO:
    
    * What to do with really large numbers outside of range, like 1337.0e25
    * Do we care about the Rails number_to_human_size API?
      Maybe create a separate SI API and implement Rails' number_to_human_size using that.

--- lib/number/human.ex
+++ lib/number/human.ex
@@ -61,4 +61,87 @@ defmodule Number.Human do
       number <> " " <> unquote(label)
     end
   end
+
+  @doc """
+  Format numbers using SI notation
+
+  ## Parameters
+
+  * `number` - A value to convert. Can be any value that implements
+    `Number.Conversion.to_float/1`.
+
+  * `options` - A keyword list of options. See the documentation below for all
+    available options.
+
+  ## Options
+
+  * `:base` - Use 1024 if you wish to format bytes.  Default: 1000
+
+  * `:separator` - The string to place between the scaled number and the
+                   prefix + unit. Perhaps you want a space here. Default: ""
+
+  * `:unit` - The unit of measurement, e.g. "M" for Meters. Default: ""
+
+  * `:precision` - The number of decimal places to include. Default: 2
+
+  ## Examples
+
+      iex> Number.Human.number_to_si(1210000000, unit: "W")
+      "1.21GW"
+
+      iex> Number.Human.number_to_si(1210000000, unit: "W", precision: 1)
+      "1.2GW"
+
+      iex> Number.Human.number_to_si(1210000000, unit: "W", precision: 3, separator: " ")
+      "1.210 GW"
+
+      iex> Number.Human.number_to_si(1210000000)
+      "1.21G"
+
+  """
+  @prefixes %{
+    8 =>  "Y",   # yotta
+    7 =>  "Z",   # zetta
+    6 =>  "E",   # exa
+    5 =>  "P",   # peta
+    4 =>  "T",   # tera
+    3 =>  "G",   # giga
+    2 =>  "M",   # mega
+    1 =>  "k",   # kilo
+    0 =>  "",
+    -1 => "m",   # milli
+    -2 => "µ",   # micro
+    -3 => "n",   # nano
+    -4 => "p",   # pico
+    -5 => "f",   # femto
+    -6 => "a",   # atto
+    -7 => "z",   # zepto
+    -8 => "y"    # ycoto
+  }
+  def number_to_si(number, options \\ [])
+  def number_to_si(number, options) do
+    options = Dict.merge(number_to_si_config, options)
+    number_to_si(number, options[:base], options[:separator], options[:unit], options[:precision])
+  end
+  defp number_to_si(number, base, separator, unit, precision) do
+    if number == 0 do
+      exp = 0
+    else
+      exp = :math.log(number) / :math.log(base) |> Float.floor |> trunc
+    end
+    prefix = @prefixes[exp]
+    scaled_number = number / :math.pow(base, exp)
+    display_number = Float.to_string(scaled_number, decimals: precision)
+    display_number <> separator <> prefix <> unit
+  end
+  defp number_to_si_config do
+    defaults = [
+      base: 1000,
+      separator: "",
+      unit: "",
+      precision: 2
+    ]
+
+    Dict.merge(defaults, Application.get_env(:number, :si, []))
+  end
 end
