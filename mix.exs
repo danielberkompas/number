5b52ff77d8d3769886f5807905de3564358d305e
Daniel Berkompas <himself@danielberkompas.com>
2015-12-15 20:34:09 -0800

    Upgrade to ExDoc 0.11.1

--- mix.exs
+++ mix.exs
@@ -13,25 +13,13 @@ defmodule Currency.Mixfile do
      docs: docs]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:ex_doc, github: "elixir-lang/ex_doc", only: :docs},
+      {:ex_doc, "~> 0.11", only: :docs},
       {:inch_ex, "~> 0.3", only: :docs}
     ]
   end
