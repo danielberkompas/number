6a532f522f44845d66df8c6fff3a15270778cd52
Daniel Berkompas <himself@danielberkompas.com>
2017-01-17 12:15:52 -0800

    Bump version to 0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Number.Mixfile do
   def project do
     [app: :number,
      description: "Convert numbers to various string formats, such as currency",
-     version: "0.5.0",
+     version: "0.5.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
