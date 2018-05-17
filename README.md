b9914aa8977b99d20857ae630c1725cc1b9c82ab
Daniel Berkompas <himself@danielberkompas.com>
2018-05-17 13:12:21 -0700

    Bump version to 0.5.7

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Get it from Hex:
 
 ```elixir
 defp deps do
-  [{:number, "~> 0.5.6"}]
+  [{:number, "~> 0.5.7"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Number.Mixfile do
     [
       app: :number,
       description: "Convert numbers to various string formats, such as currency",
-      version: "0.5.6",
+      version: "0.5.7",
       elixir: "~> 1.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
